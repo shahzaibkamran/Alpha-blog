88a6af15604ecee42ec98360f73c480d00b412c5
symmetrically <sadanmasroor87@gmail.com>
2022-02-21 23:48:00 +0500

    added new action method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
   def index
-    render html:"hello world!"
+    @articles =  Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article.save
+    redirect_to @article
   end
 end
